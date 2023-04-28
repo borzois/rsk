d4195cd379349dbf8993ca8cdaeb4a8b3285e361
thebiggestthe <denischebac444@gmail.com>
2023-04-28 21:45:50 +0200

    booru command implementation

--- src/commands/booru.rs
+++ src/commands/booru.rs
@@ -2,25 +2,35 @@ use serenity::framework::standard::macros::command;
 use serenity::framework::standard::{Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
-use serenity::utils::MessageBuilder;
+
+use booru_rs::client::gelbooru::GelbooruClient;
+use booru_rs::client::generic::BooruClient;
+use booru_rs::client::generic::BooruBuilder;
+use booru_rs::model::gelbooru::GelbooruRating;
+use booru_rs::model::gelbooru::GelbooruSort;
+
 
 #[command]
 pub async fn booru(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let mut response = MessageBuilder::new();
-    response
-        .push("user ")
-        .push_italic_safe(&msg.author.name)
-        .push(" has requested ");
+    let mut _posts = GelbooruClient::builder();
     while !args.is_empty() {
-        response
-            .push_bold(args.single::<String>()?)
-            .push(" ");
+        _posts = _posts
+            .tag(args.single::<String>()?);
     }
-    response
-        .push_line("- shame on them!")
-        .build();
+    let _posts = _posts
+        .rating(GelbooruRating::General)
+        .sort(GelbooruSort::Score)
+        .limit(1)
+        .random(true)
+        .blacklist_tag(GelbooruRating::Explicit)
+        .get()
+        .await
+        .expect("There was an error retrieving posts from the API");
 
-    msg.channel_id.say(&ctx.http, response).await?;
+    for val in _posts.iter(){
+        let response = &val.file_url;
+        msg.channel_id.say(&ctx.http, response).await?;
+    }
 
     Ok(())
 }
