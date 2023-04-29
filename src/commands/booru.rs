90de48df44d7ffcc7027c240528c3d7e4e68a88f
borzois <36570861+borzois@users.noreply.github.com>
2023-04-29 10:33:27 +0300

    booru error checking

--- src/commands/booru.rs
+++ src/commands/booru.rs
@@ -1,5 +1,5 @@
 use serenity::framework::standard::macros::command;
-use serenity::framework::standard::{Args, CommandResult};
+use serenity::framework::standard::{Args, CommandError, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
@@ -8,6 +8,7 @@ use booru_rs::client::generic::BooruClient;
 use booru_rs::client::generic::BooruBuilder;
 use booru_rs::model::gelbooru::GelbooruRating;
 use booru_rs::model::gelbooru::GelbooruSort;
+use tracing::error;
 
 
 #[command]
@@ -24,12 +25,20 @@ pub async fn booru(ctx: &Context, msg: &Message, mut args: Args) -> CommandResul
         .random(true)
         .blacklist_tag(GelbooruRating::Explicit)
         .get()
-        .await
-        .expect("There was an error retrieving posts from the API");
+        .await;
+        // .expect("There was an error retrieving posts from the API");
 
-    for val in _posts.iter(){
-        let response = &val.file_url;
-        msg.channel_id.say(&ctx.http, response).await?;
+    match _posts {
+        Ok(posts) => {
+            for val in posts.iter(){
+                let response = &val.file_url;
+                msg.channel_id.say(&ctx.http, response).await?;
+            }
+        },
+        Err(error) => {
+            msg.channel_id.say(&ctx.http,"i couldn't find that!").await?;
+            error!("{}", error.to_string())
+        }
     }
 
     Ok(())
